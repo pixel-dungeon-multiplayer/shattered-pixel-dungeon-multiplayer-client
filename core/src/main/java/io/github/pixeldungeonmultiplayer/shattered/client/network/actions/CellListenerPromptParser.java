c727270d7905073165e3166d528debc6e28526cf
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-28 00:28:20 +0300

    fix prompt text parsing

--- core/src/main/java/io/github/pixeldungeonmultiplayer/shattered/client/network/actions/CellListenerPromptParser.java
+++ core/src/main/java/io/github/pixeldungeonmultiplayer/shattered/client/network/actions/CellListenerPromptParser.java
@@ -1,5 +1,7 @@
 package io.github.pixeldungeonmultiplayer.shattered.client.network.actions;
 
+import io.github.pixeldungeonmultiplayer.common.localizedstring.LocalizedString;
+import io.github.pixeldungeonmultiplayer.shattered.client.network.JsonStringHelper;
 import io.github.pixeldungeonmultiplayer.shattered.client.network.ParseThread;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import org.json.JSONObject;
@@ -7,6 +9,6 @@ import org.json.JSONObject;
 public class CellListenerPromptParser implements ActionParser {
     @Override
     public void parse(ParseThread parseThread, JSONObject action) {
-        GameScene.defaultCellListener.setCustomPrompt(action.optString("prompt", null));
+        GameScene.defaultCellListener.setCustomPrompt(JsonStringHelper.optString(action, "prompt", null));
     }
 }
